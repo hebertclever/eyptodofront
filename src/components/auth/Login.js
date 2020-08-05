63ab9fd7552b15f59a15843ddfa2856f27333b0c
hebertclever <64936740+hebertclever@users.noreply.github.com>
2020-08-05 17:28:47 -0300

    login and signup working

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,19 +1,24 @@
 import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
-
 import authApi from "../../apis/auth";
 
 const Login = (props) => {
   const history = useHistory();
 
   const [user, setUser] = useState({ username: "", password: "" });
-
-  async function handleSubmit(data) {
+  function handleChange(event) {
+    setUser({
+      ...user,
+      [event.currentTarget.name]: event.currentTarget.value,
+    });
+  }
+  async function handleSubmit(event) {
+    event.preventDefault();
     try {
       // Colocando uma string em branco como URL, pois os dados que queremos inserir via POST sao sempre o segundo parametro do metodo "post" do Axios
-      const result = await authApi.post("/login", data);
+      const result = await authApi.post("/login", user);
 
-      props.setUser({ ...result.data });
+      setUser({ ...result.data });
 
       localStorage.setItem("loggedInUser", JSON.stringify({ ...result.data }));
       // Redirecionar de volta para lista de Projetos
@@ -27,6 +32,34 @@ const Login = (props) => {
     <div>
       <h1>Login</h1>
       <hr></hr>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <input
+            placeholder="Username"
+            type="text"
+            className="form-control form-control-lg"
+            id="authUsernameField"
+            name="username"
+            onChange={handleChange}
+            value={user.username}
+          />
+        </div>
+        <div className="form-group">
+          <input
+            placeholder="Password"
+            type="password"
+            className="form-control form-control-lg"
+            name="password"
+            id="authPasswordField"
+            onChange={handleChange}
+            value={user.password}
+          />
+        </div>
+        <button type="submit" className="btn btn-lg btn-primary">
+          Login
+        </button>
+        <p></p>
+      </form>
     </div>
   );
 };
--- src/components/auth/Signup.js
+++ src/components/auth/Signup.js
@@ -1,11 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import authApi from "../../apis/auth";
 
-function Signup() {
+const Signup = () => {
+  const history = useHistory();
+
+  const [user, setUser] = useState({ name: "", username: "", password: "" });
+  function handleChange(event) {
+    setUser({
+      ...user,
+      [event.currentTarget.name]: event.currentTarget.value,
+    });
+  }
+  async function handleSubmit(event) {
+    event.preventDefault();
+    try {
+      // Colocando uma string em branco como URL, pois os dados que queremos inserir via POST sao sempre o segundo parametro do metodo "post" do Axios
+      const result = await authApi.post("/sign-up", user);
+
+      // Redirecionar de volta para lista de Projetos
+      history.push("/login");
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
   return (
     <div>
       <h1>Signup</h1>
+      <hr></hr>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <input
+            placeholder="Name"
+            type="text"
+            className="form-control form-control-lg"
+            id="authUsernameField"
+            name="name"
+            onChange={handleChange}
+            value={user.name}
+          />
+        </div>
+        <div className="form-group">
+          <input
+            placeholder="Username"
+            type="text"
+            className="form-control form-control-lg"
+            id="authUsernameField"
+            name="username"
+            onChange={handleChange}
+            value={user.username}
+          />
+        </div>
+        <div className="form-group">
+          <input
+            placeholder="Password"
+            type="password"
+            className="form-control form-control-lg"
+            name="password"
+            id="authPasswordField"
+            onChange={handleChange}
+            value={user.password}
+          />
+        </div>
+        <button type="submit" className="btn btn-lg btn-primary">
+          Signup
+        </button>
+      </form>
     </div>
   );
-}
+};
 
 export default Signup;
